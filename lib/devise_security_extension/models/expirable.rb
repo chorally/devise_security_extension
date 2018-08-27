2286a60fae76fdacadbd1229eb11e661bece7326
Davide Santangelo <davide.santangelo@gmail.com>
2018-08-27 16:15:38 +0200

    Update expirable.rb

--- lib/devise_security_extension/models/expirable.rb
+++ lib/devise_security_extension/models/expirable.rb
@@ -27,6 +27,8 @@ module Devise
       #
       # @return [bool]
       def expired?
+        # if never_expire is true, the user never expire
+        return false if self.never_expire?
         # expired_at set (manually, via cron, etc.)
         return self.expired_at < Time.now.utc unless self.expired_at.nil?
         # if it is not set, check the last activity against configured expire_after time range
